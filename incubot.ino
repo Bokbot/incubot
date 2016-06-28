cac0eba0523432e14ed0eead9a84a5f6e5251479
Josh Cox <josh@webhosting.coop>
2016-06-28 18:03:35 -0500

    watchdog

--- incubot.ino
+++ incubot.ino
@@ -273,7 +273,8 @@ void loop(void)
   }
   delay(dht.getMinimumSamplingPeriod());
  
-  if(checkThrottle( sensorthrottle, sensorwatchdog, sensorwatchdogLimit )){
+ // if(checkThrottle( sensorthrottle, sensorwatchdog, sensorwatchdogLimit )){
+  if(sensorwatchdog > 30){
 
     sensorthrottle = (millis() + sensorInterval); 
     sensorwatchdog = 0;
